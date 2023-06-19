f4b00773b2b5f7a0b3cc8eb5d9fd5e1d6a617cad
Thiago Fernandes <falecomobranco@yahoo.com.br>
2023-06-19 20:35:03 -0300

    first commit

---
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    color: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
