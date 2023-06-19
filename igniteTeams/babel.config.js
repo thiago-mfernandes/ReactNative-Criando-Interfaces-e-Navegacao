522e5e7b6507f8092341128c2233a5d3c2151492
Thiago Fernandes <falecomobranco@yahoo.com.br>
2023-06-19 20:43:35 -0300

    feat: @alias configured with babel plugin

--- igniteTeams/babel.config.js
+++ igniteTeams/babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  /**inserir este plugin */
+  'plugins': [
+    'module-resolver', {
+      /**indicar qual é a pasta raiz */
+      root: ['./src'],
+      /**indicar uma alias, para cada pasta */
+      alias: {
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@routes': './src/routes',
+        '@screens': './src/screens',
+        '@storage': './src/storage',
+        '@theme': './src/theme',
+        '@utils': './src/utils',
+      },
+    },
+  ],
 };
